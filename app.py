74a1388ab529f81d5bb6c3191aec40c760638797
samirdas0572 <samirdas0572@gmail.com>
2026-04-26 02:51:17 +0530

    updated app with port config
    :wq#

--- app.py
+++ app.py
@@ -431,4 +431,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
